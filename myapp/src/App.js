e63e0879786225372d2aff1f540c339fdca82b0f
YS <ys.heo.korea@gmail.com>
2023-01-23 02:08:50 +0900

    Update App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -9,14 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>This is deploy by Git Page with Jekyll</p>
       </header>
     </div>
   );
